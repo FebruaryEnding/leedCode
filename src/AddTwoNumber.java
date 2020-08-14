56357ec6ba1e3bdd3737536d59db114d1188255c
yaohuaiying <867160589@qq.com>
2020-08-14 14:31:05 +0800

    add two number3

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -22,8 +22,95 @@ import java.util.List;
 public class AddTwoNumber {
 
 
+    /**
+     * 这应该是最简陋的通过方法了
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        boolean end = true;
+        Integer value1 = 0;
+        Integer value2 = 0;
+        int jinwei = 0;
+        List<ListNode> nodes = new ArrayList<>();
+        do {
+            if (l1 == null) {
+                value1 = 0;
+            } else {
+                int val1 = l1.val;
+                value1 = val1 ;
+            }
+            if (l2 == null) {
+                value2 = 0 ;
+            } else {
+                int val2 = l2.val;
+                value2 = val2;
+            }
 
+            if (null == l1 && null == l2) {
+                end = false;
+            } else {
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
+            }
+
+
+
+            //这里做每个位的相加结果
+            int sum = value1  + value2;
+            //加进位
+            sum = sum + jinwei;
+            if(sum > 9){
+                jinwei = 1;
+                nodes.add(new ListNode(sum-10));
+                if(end == false ){
+                    nodes.add(new ListNode(1));
+                }
+            }else {
+                jinwei = 0;
+
+                if(end == false){
+                    if( sum == 0){
+
+                    }else {
+                        nodes.add(new ListNode(sum));
+                    }
+                }else {
+                    nodes.add(new ListNode(sum));
+                }
+
+            }
+
+
+        } while (end);
+
+
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes.get(i).next = nodes.get(i + 1);
+        }
+        return nodes.get(0);
+    }
+
+
+    public static void main(String[] args) {
+        AddTwoNumber addTwoNumber = new AddTwoNumber();
+
+        ListNode listNode = new ListNode(2);
+
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(3);
+//        ListNode listNodeX = new ListNode(1);
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+//        listNode2.next = listNodeX;
+        ListNode listNode3 = new ListNode(5);
+        ListNode listNode4 = new ListNode(6);
+        ListNode listNode5 = new ListNode(4);
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        ListNode listNode6 = addTwoNumber.addTwoNumbers(listNode, listNode3);
+        System.out.println("-");
     }
 
 
@@ -76,25 +163,6 @@ public class AddTwoNumber {
     }
 
 
-    public static void main(String[] args) {
-        AddTwoNumber addTwoNumber = new AddTwoNumber();
-
-        ListNode listNode = new ListNode(2);
-
-        ListNode listNode1 = new ListNode(4);
-        ListNode listNode2 = new ListNode(3);
-        ListNode listNodeX = new ListNode(1);
-        listNode.next = listNode1;
-        listNode1.next = listNode2;
-        listNode2.next = listNodeX;
-        ListNode listNode3 = new ListNode(5);
-        ListNode listNode4 = new ListNode(6);
-        ListNode listNode5 = new ListNode(4);
-        listNode3.next = listNode4;
-        listNode4.next = listNode5;
-        ListNode listNode6 = addTwoNumber.addTwoNumbers2(listNode, listNode3);
-        System.out.println("-");
-    }
 }
 
 
