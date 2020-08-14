4326354ec0640b3f91ac8aef03546e0ce8112dc3
yaohuaiying <867160589@qq.com>
2020-08-14 14:05:11 +0800

    add two number2

--- src/AddTwoNumber.java
+++ src/AddTwoNumber.java
@@ -1,4 +1,6 @@
-import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -19,8 +21,20 @@ import java.math.BigDecimal;
  */
 public class AddTwoNumber {
 
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        boolean end = false;
+
+    }
+
+
+    /**
+     * 原来不能用BigInteger 那只能自己做个累加咯 我吐了
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        boolean end = true;
         String value1 = "";
         String value2 = "";
         do {
@@ -36,23 +50,50 @@ public class AddTwoNumber {
                 int val2 = l2.val;
                 value2 = val2 + value2;
             }
-            if (l1.next != null || l2.next != null) {
-
+            if (null == l1 && null == l2) {
+                end = false;
             } else {
-                end = true;
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
             }
 
         } while (end);
-        Integer integer = Integer.valueOf(value1);
-        Integer integer1 = Integer.valueOf(value2);
-        int result = integer + integer1;
+        BigInteger integer = new BigInteger(value1);
+        BigInteger integer1 = new BigInteger(value2);
 
-        return null;
+        BigInteger result = integer .add(integer1);
+        String resultStr = result + "";
+
+
+        List<ListNode> nodes = new ArrayList<>();
+        for (int i = resultStr.length() - 1; i >= 0; i--) {
+            nodes.add(new ListNode(Integer.valueOf(resultStr.charAt(i) + "")));
+        }
+        for (int i = 0; i < nodes.size() - 1; i++) {
+            nodes.get(i).next = nodes.get(i + 1);
+        }
+        return nodes.get(0);
     }
 
 
     public static void main(String[] args) {
-        BigDecimal bigDecimal = new BigDecimal("0123");
+        AddTwoNumber addTwoNumber = new AddTwoNumber();
+
+        ListNode listNode = new ListNode(2);
+
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNodeX = new ListNode(1);
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNodeX;
+        ListNode listNode3 = new ListNode(5);
+        ListNode listNode4 = new ListNode(6);
+        ListNode listNode5 = new ListNode(4);
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        ListNode listNode6 = addTwoNumber.addTwoNumbers2(listNode, listNode3);
+        System.out.println("-");
     }
 }
 
